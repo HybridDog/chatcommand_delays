9200e476df1ea4a4be6eacccc43289fa48c6fb43
HybridDog <ovvv@web.de>
2016-05-12 19:20:05 +0200

    :boom:

---
+++ README.md
@@ -0,0 +1,22 @@
+[Mod] Chatcommand stopwatch [chatcommand_delays]
+
+This mod tells the time a chatcommand took to get executed.  
+So for example if you want to know how long it takes to set 100x100x100 nodes using worldedit,  
+you only need to have this mod, it automatically adds the delay to the message sent to the player (if no message is returned, the delay gets sent in an own line).  
+Currently the time is added in brackets at the end of the message.
+
+**Depends:** see [depends.txt](https://raw.githubusercontent.com/HybridDog/chatcommand_delays/master/depends.txt)  
+**License:** see [LICENSE.txt](https://raw.githubusercontent.com/HybridDog/chatcommand_delays/master/LICENSE.txt)  
+**Download:** [zip](https://github.com/HybridDog/chatcommand_delays/archive/master.zip), [tar.gz](https://github.com/HybridDog/chatcommand_delays/tarball/master)  
+
+Obviously Worldedit doesn't use minetest.register_chatcommand correctly  
+setting about 2.5 mio nodes may take longer for you, I use my own worldedit modifications  
+![I'm a screenshot!](https://cloud.githubusercontent.com/assets/3192173/15223716/1dbe7632-1876-11e6-9f1c-1e8178b11e96.png)
+
+It works with every chatcommand, e.g. the one of function delayer:  
+![I'm a screenshot!](https://cloud.githubusercontent.com/assets/3192173/15223395/7e263b42-1874-11e6-864a-98c18ff9df00.png)
+
+If you got ideas or found bugs, please tell them to me.
+
+[How to install a mod?](http://wiki.minetest.net/Installing_Mods)
+
---
+++ init.lua
@@ -0,0 +1,53 @@
+local load_time_start = minetest.get_us_time()
+
+
+-- only tell the time if the chatcommand took noticeable time
+local treshold = 0.01
+
+-- Override the chatcommand function of builtin to tell the time the command took
+minetest.registered_on_chat_messages[1] = function(name, message)
+	local cmd, param = string.match(message, "^/([^ ]+) *(.*)")
+	if not param then
+		param = ""
+	end
+	local cmd_def = core.chatcommands[cmd]
+	if not cmd_def then
+		return false
+	end
+	local has_privs, missing_privs = core.check_player_privs(name, cmd_def.privs)
+	if has_privs then
+		core.set_last_run_mod(cmd_def.mod_origin)
+
+		-- ## changed here
+		local t1 = minetest.get_us_time()
+		-- ##
+
+		local success, message = cmd_def.func(name, param)
+
+		-- ## and here
+		local delay = (minetest.get_us_time()-t1)/1000000
+		if delay > treshold then
+			message = (message or "") .. " (" .. delay .. " s)"
+			--" Chatcommand executed after " .. delay .. " seconds."
+		end
+		-- ##
+
+		if message then
+			core.chat_send_player(name, message)
+		end
+	else
+		core.chat_send_player(name, "You don't have permission"
+				.. " to run this command (missing privileges: "
+				.. table.concat(missing_privs, ", ") .. ")")
+	end
+	return true  -- Handled chat message
+end
+
+
+local time = (minetest.get_us_time() - load_time_start) / 1000000
+local msg = "[chatcommand_delays] loaded after ca. " .. time .. " seconds."
+if time > 0.01 then
+	print(msg)
+else
+	minetest.log("info", msg)
+end
